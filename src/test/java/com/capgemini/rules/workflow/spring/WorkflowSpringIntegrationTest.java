f2418cdcb98ae980b56222637a853dbeac20e87a
James Devaney <james.devaney@capgemini.com>
2017-03-31 15:21:23 +0100

    Simplify Spring tests

--- src/test/java/com/capgemini/rules/workflow/spring/WorkflowSpringIntegrationTest.java
+++ src/test/java/com/capgemini/rules/workflow/spring/WorkflowSpringIntegrationTest.java
@@ -10,6 +10,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.capgemini.rules.workflow.Workflow;
 import com.capgemini.rules.workflow.WorkflowContext;
+import com.capgemini.rules.workflow.WorkflowIntegrationTest.Pet;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TestConfig.class)
@@ -20,39 +21,27 @@ public class WorkflowSpringIntegrationTest {
 	
 	@Test
 	public void testCat() {		
-		WorkflowContext<Pet, String> context = new WorkflowContext<Pet, String>();
-		Pet pet = new Pet();
-		pet.hasFourLegs = true;
-		pet.mansBestFriend = false;
-		context.setInputObject(pet);
-		context.setOutputObject(new String()); // Often you might eagerly initialise this rather than rely on the workflow to do it for you
-		context.setLoggingPrefix("CorrelationId: testCat");
+		Pet pet = new Pet(true, false);
+		WorkflowContext<Pet, String> context = new WorkflowContext<Pet, String>().setInputObject(pet).setOutputObject(new String()); // Often you might eagerly initialise this rather than rely on the workflow to do it for you
+		context.setLoggingPrefix("CorrelationId: testCat"); //You might also want to set some sort of prefix to your logging e.g. to stitch together log entries for a single execution
 		workflow.execute(context);
 		assertEquals("Cat", context.getOutputObject());
 	}
 	
 	@Test
 	public void testDog() {		
-		WorkflowContext<Pet, String> context = new WorkflowContext<Pet, String>();
-		Pet pet = new Pet();
-		pet.hasFourLegs = true;
-		pet.mansBestFriend = true;
-		context.setInputObject(pet);
-		context.setOutputObject(null); // Often you might eagerly initialise this rather than rely on the workflow to do it for you
-		context.setLoggingPrefix("CorrelationId: testDog");
+		Pet pet = new Pet(true, true);
+		WorkflowContext<Pet, String> context = new WorkflowContext<Pet, String>().setInputObject(pet).setOutputObject(new String()); // Often you might eagerly initialise this rather than rely on the workflow to do it for you
+		context.setLoggingPrefix("CorrelationId: testCat"); //You might also want to set some sort of prefix to your logging e.g. to stitch together log entries for a single execution
 		workflow.execute(context);
 		assertEquals("Dog", context.getOutputObject());
 	}
 	
 	@Test
 	public void testGoldfish() {		
-		WorkflowContext<Pet, String> context = new WorkflowContext<Pet, String>();
-		Pet pet = new Pet();
-		pet.hasFourLegs = false;
-		pet.mansBestFriend = false;
-		context.setInputObject(pet);
-		context.setOutputObject(null); // Often you might eagerly initialise this rather than rely on the workflow to do it for you
-		context.setLoggingPrefix("CorrelationId: testGoldfish");
+		Pet pet = new Pet(false, false);
+		WorkflowContext<Pet, String> context = new WorkflowContext<Pet, String>().setInputObject(pet).setOutputObject(new String()); // Often you might eagerly initialise this rather than rely on the workflow to do it for you
+		context.setLoggingPrefix("CorrelationId: testCat"); //You might also want to set some sort of prefix to your logging e.g. to stitch together log entries for a single execution
 		workflow.execute(context);
 		assertEquals("Goldfish", context.getOutputObject());
 	}
@@ -60,5 +49,9 @@ public class WorkflowSpringIntegrationTest {
 	public class Pet {
 		public boolean hasFourLegs;
 		public boolean mansBestFriend;
+		public Pet(boolean hasFourLegs, boolean mansBestFriend){
+			this.hasFourLegs = hasFourLegs;
+			this.mansBestFriend = mansBestFriend;
+		}
 	}
 }
